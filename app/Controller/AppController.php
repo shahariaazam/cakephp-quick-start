c4bf22ed420a1552fd83c5bb051545519b5f75a9
Shaharia Azam <shaharia.azam@gmail.com>
2013-09-13 15:52:18 +0600

    Session added as application-wide component

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -33,6 +33,6 @@ App::uses('Controller', 'Controller');
  */
 class AppController extends Controller {
 
-    public $components = array('DebugKit.Toolbar');
+    public $components = array('Session', 'DebugKit.Toolbar');
 
 }
